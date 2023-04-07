29722795f0666826626f2d6e70f3de9f3a48c566
Diptabh Medhi <tinumedhi@gmail.com>
2023-04-07 07:57:57 +0530

    2nd - installed the image picker

--- app/src/main/java/com/example/gdsc2/MainActivity.kt
+++ app/src/main/java/com/example/gdsc2/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.gdsc2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.cast.framework.media.ImagePicker
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fab = findViewById<ExtendedFloatingActionButton>(R.id.add_button)
+
+        fab?.setOnClickListener {
+
+            //________________________________________________________________________________
+//                Previous Codes
+//val iGallery = Intent(Intent.ACTION_PICK)
+//                iGallery.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+//                    startActivityForResult(iGallery, 1000)
+            //_______________________________________________________________________________
+            com.github.dhaval2404.imagepicker.ImagePicker.with(this).crop().
+            compress(250).
+            maxResultSize(600,600)
+                .start(1000)
+
+        }
     }
 }
