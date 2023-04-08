0c3e9ce07538de8539fd0ec728aee51d9a438687
Diptabh Medhi <tinumedhi@gmail.com>
2023-04-08 15:07:31 +0530

    6th - few errors but still working

--- app/src/main/java/com/example/gdsc2/MainActivity.kt
+++ app/src/main/java/com/example/gdsc2/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.gdsc2
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.media.Image
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Adapter
 import android.widget.ImageView
 import android.widget.Toast
@@ -12,18 +15,29 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gdsc2.datahub.UserData
 import com.example.gdsc2.datahub.adapter
+import com.example.gdsc2.ml.GdscModel
 import com.google.android.gms.cast.framework.media.ImagePicker
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import kotlinx.coroutines.delay
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var recylerView: RecyclerView
     private lateinit var datalist: ArrayList<UserData>
+    private var imageProcessor = ImageProcessor.Builder().add(ResizeOp(256, 256,ResizeOp.ResizeMethod.BILINEAR)).build()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //image processor
 
         val fab = findViewById<ExtendedFloatingActionButton>(R.id.add_button)
         recylerView = findViewById(R.id.recyclerView)
@@ -47,14 +61,50 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
+
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_OK) {
             //Image Uri will not be null for RESULT_OK
             val uri: Uri = data?.data!!
+
+
+            var bitmap : Bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver,uri)
+            var tensorImage = TensorImage(DataType.FLOAT32)
+            tensorImage.load(bitmap)
+
+            tensorImage = imageProcessor.process(tensorImage)
+
+            val model = GdscModel.newInstance(this)
+
+
+// Creates inputs for reference.
+            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 256, 256, 3), DataType.FLOAT32)
+
+            inputFeature0.loadBuffer(tensorImage.buffer)
+
+// Runs model inference and gets result.
+            val outputs = model.process(inputFeature0)
+            val outputFeature0 = outputs.outputFeature0AsTensorBuffer.floatArray
+
+            var maxId = 0
+            outputFeature0.forEachIndexed{
+                index, fl ->
+                if(outputFeature0[maxId]<fl){
+                    maxId = index
+                }
+            }
+
+
+// Releases model resources if no longer used.
+            model.close()
+
             datalist.add(UserData(uri,"test","test"))
             val intent = Intent(this@MainActivity, TestingActivity::class.java)
             intent.putExtra("imageUri", uri)
+            intent.putExtra("disease",maxId)
             startActivity(intent)
             recylerView.adapter = adapter(datalist)
 
